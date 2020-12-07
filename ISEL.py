e8ef8b301556ab7ab022e996353c123a97acdf97
Skyedragon <D.Dmytriiev@gsi.de>
2020-12-07 11:12:27 +0000

    ISEL.send(cmd) awaits answer from the motor driver and returns answer as string

--- ISEL.py
+++ ISEL.py
@@ -1,5 +1,6 @@
 import serial
 import sys
+import time
 
 class ISEL:
     def __init__(self, baudrate, bytesize, parity, stopbits, timeout):
@@ -14,9 +15,15 @@ class ISEL:
         print('Port: ' ,self.ser.name)         # check which port was really used
         self.ser.write( (command + '\r').encode('utf-8') )     # write a string
         print( 'Command: ', (command + '\r').encode('utf-8') )
-        print('Answer:', self.ser.readline())
+        while (True):
+            answer = self.ser.readline()
+            if answer != b'':
+                break
+        print('Answer:', answer)
         self.ser.close()             # close port
-
+        return answer.decode('utf-8')
+    
 if __name__ == "__main__":
     ISEL = ISEL(19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 1)
-    ISEL.send(sys.argv[1])
+    a = ISEL.send(sys.argv[1])
+    print(a)
